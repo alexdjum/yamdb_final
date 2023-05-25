44b3ac9dee06ff0cc88aab08c1dec3d57f8c7197
Александр Джуманов <killer260598@mail.ru>
2023-05-25 15:12:02 +0300

    Error flake8

--- api_yamdb/api/titles_mixins.py
+++ api_yamdb/api/titles_mixins.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.mixins import (CreateModelMixin,
-                                   DestroyModelMixin,
-                                   ListModelMixin)
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin
+)
 
 
 class CreateListDestroyViewSet(
